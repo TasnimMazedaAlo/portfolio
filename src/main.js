7df392c00e8c7c8fc477466e2440e3c4fa9d80ec
TasnimMazedaAlo <tmazedaalo@gmail.com>
2024-05-28 21:17:03 +0600

    again

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 
+let navMenuButtonOn = document.getElementById("navMenuButtonOn");
+let navMenuButtonOff = document.getElementById("navMenuButtonOff");
+let navContent = document.getElementById("navContent");
+
+
+function showMenu() {
+    navMenuButtonOn.classList.toggle("hidden");
+    navMenuButtonOff.classList.toggle("hidden");
+    navContent.classList.toggle("hidden")
+    
+}
